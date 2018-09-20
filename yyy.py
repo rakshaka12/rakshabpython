97a8ff90170ae547df45de79fc6ffaf3866c78ec
rakshaka12 <43023136+rakshaka12@users.noreply.github.com>
2018-09-20 09:01:51 +0530

    Add files via upload

---
+++ yyy.py
@@ -0,0 +1,22 @@
+enter your percentage46
+E
+
+
+enter your percentage87
+B
+
+enter your percentage67
+D
+
+enter your percentage98
+A
+
+enter your percentage54
+E
+
+enter your percentage77
+C
+
+enter your percentage32
+fail
+
