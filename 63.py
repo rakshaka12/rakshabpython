e335c526a5e90b7e2e0567eb93e2231efc3816dc
rakshaka12 <43023136+rakshaka12@users.noreply.github.com>
2018-09-25 11:39:38 +0530

    Add files via upload

---
+++ 63.py
@@ -0,0 +1,19 @@
+enter some value:t
+bangalore temperature is 25
+enter some value:t
+bangalore temperature is 26
+enter some value:t
+bangalore temperature is 27
+enter some value:t
+bangalore temperature is 30
+enter some value:t
+bangalore temperature is 30
+enter some value:t
+bangalore temperature is 25
+enter some value:t
+bangalore temperature is 27
+enter some value:t
+bangalore temperature is 25
+enter some value:
+bye
+
