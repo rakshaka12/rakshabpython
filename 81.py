c52165a5a48fc4c66ad64bbd6a4f262f4be855d2
rakshaka12 <43023136+rakshaka12@users.noreply.github.com>
2018-10-04 10:10:40 +0530

    Add files via upload

---
+++ 81.py
@@ -0,0 +1,16 @@
+welcome to rock paper scissor
+3 = scissor
+enter k for rock,a for paper,s for scissork
+p wins
+
+welcome to rock paper scissor
+3 = scissor
+enter k for rock,a for paper,s for scissora
+c wins
+
+welcome to rock paper scissor
+1 = rock
+enter k for rock,a for paper,s for scissors
+c wins
+computer wins
+
