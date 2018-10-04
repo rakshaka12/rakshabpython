754fb0ad997815f5ebdc6277374386c56358c6a4
rakshaka12 <43023136+rakshaka12@users.noreply.github.com>
2018-10-04 10:11:39 +0530

    Add files via upload

---
+++ 73.py
@@ -0,0 +1,122 @@
+welcome to the game snake and ladder
+enter d to roll the diced
+r value is 1
+score is 1
+try again
+enter d to roll the diced
+r value is 2
+score is 3
+try again
+enter d to roll the diced
+r value is 5
+score is 8
+climb ladder
+enter d to roll the diced
+r value is 4
+score is 41
+try again
+enter d to roll the diced
+r value is 3
+score is 44
+try again
+enter d to roll the diced
+r value is 4
+score is 48
+try again
+enter d to roll the diced
+r value is 1
+score is 49
+try again
+enter d to roll the diced
+r value is 6
+score is 55
+try again
+enter d to roll the diced
+r value is 3
+score is 58
+try again
+enter d to roll the diced
+r value is 1
+score is 59
+try again
+enter d to roll the diced
+r value is 3
+score is 62
+try again
+enter d to roll the diced
+r value is 6
+score is 68
+try again
+enter d to roll the diced
+r value is 4
+score is 72
+try again
+enter d to roll the diced
+r value is 2
+score is 74
+try again
+enter d to roll the diced
+r value is 3
+score is 77
+try again
+enter d to roll the diced
+r value is 5
+score is 82
+try again
+enter d to roll the diced
+r value is 3
+score is 85
+try again
+enter d to roll the diced
+r value is 2
+score is 87
+try again
+enter d to roll the diced
+r value is 2
+score is 89
+snake bite
+enter d to roll the diced
+r value is 2
+score is 72
+try again
+enter d to roll the diced
+r value is 1
+score is 73
+try again
+enter d to roll the diced
+r value is 1
+score is 74
+try again
+enter d to roll the diced
+r value is 5
+score is 79
+try again
+enter d to roll the diced
+r value is 5
+score is 84
+try again
+enter d to roll the diced
+r value is 1
+score is 85
+try again
+enter d to roll the diced
+r value is 2
+score is 87
+try again
+enter d to roll the diced
+r value is 5
+score is 92
+try again
+enter d to roll the diced
+r value is 3
+score is 95
+try again
+enter d to roll the diced
+r value is 1
+score is 96
+try again
+enter d to roll the diced
+r value is 5
+score is 101
+try again
+
